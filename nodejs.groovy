5a5d9893e2eea8a4c57cc2e9d61ef21ab89d1873
EC2 Default User <ec2-user@ip-172-31-29-227.us-west-2.compute.internal>
2021-07-13 07:26:26 +0000

    fixed

--- nodejs.groovy
+++ nodejs.groovy
@@ -2,7 +2,7 @@ job('NodeJS example') { // Job NAME
     scm { // Configure Source control management 
         git('git://github.com/bensgith/docker-cicd.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('bensgith@github.com)
+            node / gitConfigEmail('bensgith@github.com')
         }
     }
     triggers { // Configure when to check for changes 

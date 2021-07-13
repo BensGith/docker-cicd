03d0aeaa2bb7f3ecb743b4614b6adddbe0449577
EC2 Default User <ec2-user@ip-172-31-29-227.us-west-2.compute.internal>
2021-07-13 08:39:16 +0000

    added app files

--- nodejs.groovy
+++ nodejs.groovy
@@ -8,12 +8,17 @@ job('NodeJS example') { // Job NAME
     triggers { // Configure when to check for changes 
         scm('H/5 * * * *')
     }
-    wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
-                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
-    }
     steps { // what steps to take 
-        shell("npm install")
+        dockerBuildAndPublish {
+            repositoryName(benmali94/ex2)
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+
+
     }
 }
 
